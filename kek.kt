cadb139e5526e4491fc033ff6d2fce80bffd192c
Pochka228D2 <cca77413@gmail.com>
2025-11-17 11:36:34 +0400

    lolkr=ek

---
+++ kek.kt
@@ -0,0 +1,14 @@
+fun main() {
+    val kek1:String= "сирень"
+    val kek2:String= "жасмин"
+    val kek3:String= "шиповник"
+    val kek4:String= "одуванчик"
+    val kek5:String= "розы"
+    println("Выберите куст:{$kek1,$kek2,$kek3,$kek4,$kek5}" )
+        val kek: String= readln()
+     when
+    $kek1 -> println("100")
+    $kek2 -> println("120")
+    $kek3 -> println("150")
+    else -> println("80")
+}
